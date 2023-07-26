738d0d556c6bcd4a36ee29778fe77a9e0d6c3459
jwblau2 <jeremy@abundantly.com>
2023-07-25 21:31:01 -0600

    simplified the code / removed unnecessary notes

--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,30 @@
 
-
-
-
 function getApi() {
     // fetch request gets a list of all the repos for the node.js organization
     var requestUrl = 'https://api.harvardartmuseums.org/image/465905?apikey=234ad53e-01dc-495c-8f0d-836ba0af5547';
 
     fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data)})
-      
-      //Get art image
-    .then(function (data) {
-      // Get the baseimageurl from the API response
-      var baseImageUrl = data.baseimageurl;
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
 
-      // Create a new image element
-      var imageElement = document.createElement('img');
-
-      // Set the src attribute of the image to the baseimageurl
-      imageElement.src = baseImageUrl;
-
-      // Get the image-box element from the DOM
-      var imageBox = document.querySelector('.image-box');
-
-      // Append the image element to the image-box
-      imageBox.appendChild(imageElement);
-
-      // Get the description from the API response
-      var description = data.description;
-
-      // Create a new paragraph element for the description
-      var descriptionElement = document.createElement('p');
-
-      // Set the text content of the paragraph to the description
-      descriptionElement.textContent = description;
-
-      // Get the art-title-description-box element from the DOM
-      var descriptionBox = document.querySelector('.art-title-description-box');
-
-      // Append the description paragraph element to the description-box
-      descriptionBox.appendChild(descriptionElement);
-    });
+          //Get art image
+          var baseImageUrl = data.baseimageurl;
+          var imageElement = document.createElement('img');
+          imageElement.src = baseImageUrl;
+          var imageBox = document.querySelector('.image-box');
+          imageBox.appendChild(imageElement);
+          //Get art description
+          var description = data.description;
+          var descriptionElement = document.createElement('p');
+          descriptionElement.textContent = description;
+          var descriptionBox = document.querySelector('.art-title-description-box');
+          descriptionBox.appendChild(descriptionElement);
+        })
+        .catch(function (error) {
+          console.log('Error fetching data:', error);
+        });
     }
 getApi()
